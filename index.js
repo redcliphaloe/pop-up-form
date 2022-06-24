1a2c8ae112d831a3a274bce79b88b5a58e0c928f
redcliphaloe <redcliphaloe@gmail.com>
2022-06-24 20:49:03 +0300

    feat: add pop-up functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Connected...');
+const getInfoBtn = document.querySelector('.get-info');
+const blackOut = document.querySelector('.black-out');
+const popUp = document.querySelector('.pop-up');
+const popUpCloseBtn = document.querySelector('.pop-up__close-btn');
+
+const showPopUp = (e) => {
+  blackOut.classList.add('black-out_show');
+  popUp.classList.add('pop-up_show');
+  document.querySelector('#name').focus();
+}
+
+const closePopUp = (e) => {
+  blackOut.classList.remove('black-out_show');
+  popUp.classList.remove('pop-up_show');
+}
+
+const submitPopUp = (e) => {
+  const formElems = e.target.elements;
+
+  e.preventDefault();
+  console.log(`Sent:\nName = ${formElems.name.value}\nE-Mail = ${formElems['e-mail'].value}\nMessage = ${formElems.message.value}`);
+  closePopUp();
+}
+
+getInfoBtn.addEventListener('click', showPopUp);
+popUpCloseBtn.addEventListener('click', closePopUp);
+popUp.addEventListener('submit', submitPopUp)
